682b5f40e54e5e82adfdedd8cfe13fd7c742e617
Joao Roma <joaoricardoroma@gmail.com>
2023-09-20 00:33:53 +0100

    Docker with containers and requirements

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^w5-+#dw(e1$uyq(c8d%wkpf9q-sbphs7$(@+v87b8m*(fkw6q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'roma',
         'USER': 'root',
         'PASSWORD': '123',
-        'HOST': 'localhost',
+        'HOST': 'roma_db',
         'PORT': '3306',
     }
 }
--- project/website/views.py
+++ project/website/views.py
@@ -53,7 +53,7 @@ def customer_record(request, pk):
     if request.user.is_authenticated:
         # Look Up Records
         customer_record = Record.objects.get(id=pk)
-        return render(request, 'record.html', {'customer_record': customer_record})
+        return render(request, 'tasks.html', {'customer_record': customer_record})
     else:
         messages.success(request, "You Must Be Logged In To View That Page...")
         return redirect('home')
